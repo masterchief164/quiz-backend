65d1a40890d8aef2bcf93d12fa64bf92c61be888
masterchief164 <rishugupta2001.rg@gmail.com>
2021-06-27 00:28:11 +0530

    connected to database

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const cors = require("cors");
 const express = require("express")
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors())
 
+mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to database");
+}).catch(err => {
+    console.log(err);
+})
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started on port 3000");
+});
